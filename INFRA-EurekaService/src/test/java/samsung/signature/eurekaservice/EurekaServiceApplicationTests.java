1f580e533dcd4bbfdc7c2249fbc4c9dc6be8955d
junhochoi-dev <cjh970422@gmail.com>
2024-05-23 11:07:45 +0900

    Feat: eurekaservice init

---
+++ INFRA-EurekaService/src/test/java/samsung/signature/eurekaservice/EurekaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package samsung.signature.eurekaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
